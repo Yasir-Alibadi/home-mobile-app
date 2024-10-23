53a06e6d9fdba96129746b6f0c8349affb4f653b
YasirJA <yasir.alibadi1@gmail.com>
2024-10-22 23:45:12 -0700

    add menu items

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,75 @@
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#D93F3C",
+        // headerShown: false,
+        headerStyle: {
+          backgroundColor: "#FFFFFF",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#333333",
+        tabBarStyle: {
+          backgroundColor: "#F8F8F8",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "search" : "search-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorite"
+        options={{
+          title: "Favorite",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "heart" : "heart-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mylist"
+        options={{
+          title: "My List",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "list-sharp" : "list-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-sharp" : "person-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/favorite.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function FavoriteScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Favorite screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    // color: "#fff",
+  },
+});
